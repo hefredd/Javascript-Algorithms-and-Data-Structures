4a9bb1c3448ae994049c768347a23f129992a4b2
Fred <fr3drutto@gmail.com>
2025-07-28 22:57:11 +0300

    first commit

---
+++ GradebookApp/script.js
@@ -0,0 +1,39 @@
+function getAverage(scores) {
+  let sum = 0;
+
+  for (const score of scores) {
+    sum += score;
+  }
+
+  return sum / scores.length;
+}
+
+function getGrade(score) {
+  if (score === 100) {
+    return "A++";
+  } else if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+
+function hasPassingGrade(score) {
+  return getGrade(score) !== "F";
+}
+
+function studentMsg(totalScores, studentScore) {
+  const classAverage = getAverage(totalScores).toFixed(2); // Round to 2 decimal places
+  const studentGrade = getGrade(studentScore);
+  const status = studentGrade === "F" ? "failed" : "passed";
+
+  return `Class average: ${classAverage}. Your grade: ${studentGrade}. You ${status} the course.`;
+
+}
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 100));
